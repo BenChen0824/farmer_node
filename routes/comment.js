92bddf9c907adeb3d8c5eec050501a3c37ce3959
huimin <M9805544@gmail.com>
2022-08-15 17:38:05 +0800

    0815

--- routes/comment.js
+++ routes/comment.js
@@ -62,6 +62,54 @@ const moment = require('moment-timezone');
 //     }
 
 
+//按讚改變
+router.post('/islikedchange', async (req,res)=>{
+
+
+  const output = {
+      success: false,
+      error: '',
+  };
+  if(!req.body.customer_id || !req.body.comment_sid){
+      output.error="缺少參數"
+      res.json(output)
+  }
+
+  const sql3 = `SELECT * FROM comment_isliked WHERE customer_id=? && comment_sid=?`;
+  const [ num ] = await db.query(sql3, [req.body.customer_id,req.body.comment_sid]);
+  if (num.length <= 0) {
+    // console.log(num);
+      const sqlInsert = "INSERT INTO `comment_isliked`(`comment_sid`, `customer_id`, `isliked`) VALUES (?,?,1)"
+      const [data] = await db.query(sqlInsert,[req.body.comment_sid,req.body.customer_id]);
+      res.json(data)
+  } 
+  else
+  {
+    // console.log(num);
+  const sqlChangeIsliked = "UPDATE `comment_isliked` SET `isliked`=? WHERE customer_id=? && comment_sid=?"
+  const [data] = await db.query(sqlChangeIsliked,[(num[0].isliked===1?0:1),req.body.customer_id,req.body.comment_sid]);
+  // console.log(data[0].isliked);
+  res.json(data)}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //抓全部資料.(comment) + 商品列表(product)的商品名稱(product_name)
 router.get('/', async (req, res) => {
@@ -82,6 +130,7 @@ router.get('/', async (req, res) => {
 
 // 抓商品的SID 有的話，再去對應評論的留言
 router.post('/getproductbyname', async (req, res) => {
+  console.log(req.body);
   const sql = `SELECT c.*, p.* 
 FROM comment c
 JOIN product p
@@ -90,7 +139,7 @@ WHERE p.product_name LIKE ?
 ORDER BY c.created_at DESC
 `;
 console.log(req.body);
-const [r] = await db.query(sql, [req.body.product_name]);
+const [r] = await db.query(sql, [`%${req.body.product_name}`]);
 res.json(r);
 
 });
