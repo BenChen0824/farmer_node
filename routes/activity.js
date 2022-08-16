1e7d6cc3ffa677dc883e918e8b257f3bb1787a64
BoPin1219 <106055273+BoPin1219@users.noreply.github.com>
2022-08-16 12:10:54 +0800

    ac

--- routes/activity.js
+++ routes/activity.js
@@ -30,7 +30,44 @@ router.get('/getdata' , async (req,res)=>{
     res.json(data)
 })
 
+//確認收藏狀況
+//如果有customer_id,activity_sid 且資料庫有回傳>1的資料 代表之前有收藏過 有收藏回傳1 沒收藏回傳0 
+//如果資料庫回傳<1的資料 代表之前該帳號對該活動沒有收藏過 回傳0
+router.get('/checkisliked', async (req,res)=>{
+    
+    const sql = "SELECT * FROM `activity_isliked` WHERE customer_id=? && activity_sid=?"
+    const [data] = await db.query(sql,[req.header('customer_id'),req.header('activity_sid')]);
+    if(data.length>=1){
+        res.json(data[0].isliked)
+    } else {
+        res.json(0)
+    }
+    
+})
+router.post('/islikedchange', async (req,res)=>{
+    const output = {
+        success: false,
+        error: '',
+    };
+    if(!req.body.customer_id || !req.body.activity_sid){
+        output.error="缺少參數"
+        res.json(output)
+    }
 
+    const sql3 = `SELECT COUNT(1) num FROM activity_isliked WHERE customer_id=? && activity_sid=?`;
+    const [[{ num }]] = await db.query(sql3, [req.body.customer_id,req.body.activity_sid]);
+    if (num <= 0) {
+        const sqlInsert = "INSERT INTO `activity_isliked`(`activity_sid`, `customer_id`, `isliked`) VALUES (?,?,1)"
+        const [data] = await db.query(sqlInsert,[req.body.activity_sid,req.body.customer_id]);
+        res.json(data)
+    } 
+    else
+    {
+    const sqlChangeIsliked = "UPDATE `activity_isliked` SET `isliked`=? WHERE customer_id=? && activity_sid=?"
+    const [data] = await db.query(sqlChangeIsliked,[req.body.isliked,req.body.customer_id,req.body.activity_sid]);
+    // console.log(data[0].isliked);
+    res.json(data)}
+})
 
 //新增
 // router.post('/add', async (req, res) => {
